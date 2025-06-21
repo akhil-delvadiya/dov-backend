4e20df341c8fd9ab9ed6c4c33457b8031a978390
akhil_delvadiya <akhil@riseotech.com>
2025-06-21 16:01:38 +0530

    fix:remove serverless

--- api/index.js
+++ api/index.js
@@ -1,27 +1,10 @@
-// // api/index.js
-// const serverless = require("serverless-http");
-// const express = require("express");
-// const app = express();
-// const authRoutes = require("../routes/auth.routes");
-
-// app.use(express.json());
-// app.use("/api", authRoutes); // your /signup and /login are now /api/signup, /api/login
-
-// module.exports.handler = serverless(app);
-
-
 // api/index.js
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
-
-// TEST ROUTE to see if app loads
-app.get("/ping", (req, res) => res.json({ status: "ok ✅" }));
-
-// Load real routes
-const authRoutes = require("../routes/auth.routes"); // may crash if file missing
+const authRoutes = require("../routes/auth.route");
 
 app.use(express.json());
-app.use("/api", authRoutes);
+app.use("/api", authRoutes); // your /signup and /login are now /api/signup, /api/login
 
-module.exports.handler = serverless(app);
+module.exports = app
