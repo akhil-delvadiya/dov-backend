327388b77d2eb3facb3ea11b2587b60e9355454f
akhil_delvadiya <akhil@riseotech.com>
2025-06-21 16:16:50 +0530

    fix:quick fix

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
-const authRoutes = require("../routes/auth.route");
+const authRoutes = require("../routes/auth.routes");
 
 app.use(express.json());
 app.use("/api", authRoutes); // your /signup and /login are now /api/signup, /api/login
