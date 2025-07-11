6f93c76172ca91ec30f15475bf473fc1cb2b2f58
akhil_delvadiya <akhil@riseotech.com>
2025-07-11 17:59:22 +0530

    🐞 fix: both server port run api

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const cors = require("cors");
 
 
 const corsOptions = {
-  origin: "http://localhost:3001",
+  origin: ["http://localhost:3000", "http://localhost:3001"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
